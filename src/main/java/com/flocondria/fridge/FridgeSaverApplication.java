dfa231042ce5d6260b72fe85f98d3b12e0559f06
Laetitia-L <laetitia.laleye@gmail.com>
2018-04-15 00:32:28 +0200

    Adding some data to play with

--- src/main/java/com/flocondria/fridge/FridgeSaverApplication.java
+++ src/main/java/com/flocondria/fridge/FridgeSaverApplication.java
@@ -1,12 +1,27 @@
 package com.flocondria.fridge;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FridgeSaverApplication {
+import com.flocondria.fridge.domain.Product;
+import com.flocondria.fridge.repository.ProductRepository;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class FridgeSaverApplication implements CommandLineRunner {
+	@Autowired
+	ProductRepository productRepository;
+	
+	public static void main(String[] args){
 		SpringApplication.run(FridgeSaverApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		productRepository.save(new Product("Pain d'épices", "Bjorg"));
+		productRepository.save(new Product("Riz basmati", "Taureau ailé"));
+		productRepository.save(new Product("Chocolat 85%", "Lindt"));
+		
 	}
 }
